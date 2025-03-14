e4915f9ef0bfad75e5854d49e34dc7c9b9077ce3
Tanadon03 <tanadon.aunyart@gmail.com>
2025-03-14 18:26:14 +0700

    Save version

--- web-client-api/src/app/layout.js
+++ web-client-api/src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from '../app/Navbar'
+import "./background.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,11 +21,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <div className="el" />
           <Navbar />
-          <main className="flex-1 pt-16">{children}</main> {/* เนื้อหาทั้งหมดเริ่มใต้ Navbar */}
+          <main className="flex-1 pt-16">{children}</main>
         </ThemeProvider>
       </body>
     </html>
